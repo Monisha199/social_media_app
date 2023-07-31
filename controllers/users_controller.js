e5ab666cc2a6bec073773bdcdfcc75c689ef47e9
Monisha_19 <monishamona199@gmail.com>
2023-07-31 23:21:17 +0530

    setup static files and many more things

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,8 @@
 module.exports.profile = function(req,res){
-    return res.end('<h1>User profile</h1>')
+    // return res.end('<h1>User profile</h1>')
+    return res.render('user_profile',{
+        title:"profile"
+    })
 }
 
 module.exports.account = function(req,res){
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
 
+app.use(express.static('./assets'));
+
+app.use(expressLayouts); // need to present before routes
+app.set('layout extractStyles',true); // extract style and script from subpages into the layout
+app.set('layout extractScripts',true); // extract scripts from subpages into the layout
 app.use('/',require('./routes')); // we need to use router defined in routes , telling index.js to use routes for any requests coming 
 // after localhost url- http://127.0.0.1:8000/ 
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err){
     if(err){
